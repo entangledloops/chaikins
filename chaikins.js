89eb88ab725df431686bea88c645fd58117303e5
Stephen Dunn <dunn.stephen@gmail.com>
2016-10-15 14:19:19 -0400

    formatting cleanup

--- chaikins.js
+++ chaikins.js
@@ -1,13 +1,23 @@
-// Stephen Dunn, 11/24/13
-// This algorithm constructs a smooth curve from a set of starting points
-// See it live: https://www.khanacademy.org/computer-programming/chaikins-smoothing-algorithm/2654611482
+// @author Stephen Dunn
+// @since November 24, 2013
+//
+// This algorithm constructs a smooth curve from a random set of starting points.
+//
+// See it live: 
+// https://www.khanacademy.org/computer-programming/chaikins-smoothing-algorithm/2654611482
+//
+// Any "magic numbers" seen here were found by trial and error to look nice when rendered.
 
 var npoints = random(40,70);
 var x = [npoints];
 var y = [npoints];
-var r = random(50,255); var g = random(100,255); var b = random(100);
+var r = random(50,255); 
+var g = random(100,255); 
+var b = random(100);
 
-y[0]=400; x[0]=0;
+y[0]=400; 
+x[0]=0;
+
 for (var i=1; i<npoints; i++)
 {
     y[i] = random(400);
@@ -15,6 +25,7 @@ for (var i=1; i<npoints; i++)
 }
 
 strokeWeight(2.0);
+
 for (var i=0; i<npoints-1; i++)
 {
     stroke(r,g,b);
@@ -23,21 +34,30 @@ for (var i=0; i<npoints-1; i++)
 
 for (var s=0; s<20; s++)
 {
-    var nx = [npoints]; var ny = [npoints];
-    for (var i=0; i<npoints-1; i++)
-    {
-        var slope = (y[i+1]-y[i])/(x[i+1]-x[i]);
-        var angle = atan(slope);
-        var c = sqrt( pow(x[i+1]-x[i],2) + pow(y[i+1]-y[i],2) );
-        var w = cos(angle)*c; var h = sin(angle)*c;
+    var nx = [npoints]; 
+    var ny = [npoints];
     
-        nx[i] = x[i]+w*0.25; ny[i] = y[i]+h*0.25; 
-        nx[i+1] = x[i+1]+w*0.75; ny[i+1] = y[i+1]+h*0.75;
-    }
-    x=nx; y=ny;
     for (var i=0; i<npoints-1; i++)
     {
-        stroke(r-pow(0.7*(s+1),2),g-pow(0.7*(s+1),2),b+pow(s+1,4));
+        var slope = (y[i+1]-y[i]) / (x[i+1]-x[i]);
+        var angle = atan(slope);
+        var c = sqrt(pow(x[i+1]-x[i],2) + pow(y[i+1]-y[i],2));
+        var w = cos(angle)*c; 
+        var h = sin(angle)*c;
+    
+        nx[i] = x[i] + w*0.25; 
+        ny[i] = y[i] + h*0.25; 
+        
+        nx[i+1] = x[i+1] + w*0.75; 
+        ny[i+1] = y[i+1] + h*0.75;
+    }
+
+    x=nx; 
+    y=ny;
+
+    for (var i=0; i<npoints-1; i++)
+    {
+        stroke(r-pow(0.7*(s+1),2), g-pow(0.7*(s+1),2), b+pow(s+1,4));
         line(nx[i],ny[i],nx[i+1],ny[i+1]);
     }
 }
